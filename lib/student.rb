966f7d15184431f38a01468085afd76396cb62b8
Audra Tallman <audi.tallman@gmail.com>
2019-04-14 14:51:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,11 @@
 class Student
 
+  attr_accessor :id, :name, :grade
+  
+  def initialize (name, grade)
+  
+  
+  
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
