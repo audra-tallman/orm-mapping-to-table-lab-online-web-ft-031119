bf0efa8a6513c341e4e068d4985f6117bebbe9e3
Audra Tallman <audi.tallman@gmail.com>
2019-04-14 14:56:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,6 +10,16 @@ class Student
   
   
   def self.create_table
+    
+     sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        album TEXT
+        )
+        SQL
+    DB[:conn].execute(sql) 
+
   end 
   
   
