fe3ca22901cdf51436c84bf7d08a71395fd63c83
Audra Tallman <audi.tallman@gmail.com>
2019-04-14 14:52:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,11 @@ class Student
 
   attr_accessor :id, :name, :grade
   
-  def initialize (name, grade)
+  def initialize (id = nil, name, grade)
+    @id= id
+    @name= name
+    @grade= grade
+  end 
   
   
   
