bc6702c54638ec5e7184520319837fd925ba48f4
Audra Tallman <audi.tallman@gmail.com>
2019-04-14 14:55:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,6 +9,18 @@ class Student
   end 
   
   
+  def self.create_table
+  end 
+  
+  # def self.drop_table
+  # end 
+  
+  # def save 
+  # end 
+  
+  # def self.create 
+  # end 
+  
   
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
